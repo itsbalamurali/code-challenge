ff7b87a156a6428e415ff1de9adaca6e5152ecbd
Balamurali Pandranki <balamurali@live.com>
2022-04-13 11:18:44 +0530

    Implement handlers & add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM rust:latest AS builder
+WORKDIR /usr/src/app
+COPY Cargo.toml Cargo.lock ./
+COPY src ./src
+RUN cargo install --path .
+
+# Bundle Stage
+FROM debian:stable-slim
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends ca-certificates && \
+    rm -rf /var/lib/apt/lists/*
+RUN update-ca-certificates
+COPY --from=builder /usr/local/cargo/bin/code-challenge .
+CMD ["./code-challenge", "-a", "0.0.0.0", "-p", "8080"]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use convert_case::{Case, Casing};
 use poem::handler;
-use poem::web::Json;
+use poem::web::{Json, Query};
 use poem::{error::NotFoundError, http::StatusCode, middleware::Tracing};
-use poem::{listener::TcpListener, get, EndpointExt, Response, Route, Server};
+use poem::{get, listener::TcpListener, EndpointExt, Response, Route, Server};
+use serde::Deserialize;
 use serde_json::json;
 use tracing_subscriber::EnvFilter;
 
@@ -38,13 +40,21 @@ async fn main() -> Result<(), std::io::Error> {
         .await
 }
 
+#[derive(Deserialize)]
+struct QueryParams {
+    name: Option<String>,
+}
 
 #[handler]
-async fn helloworld_handler() -> String {
-    return "".to_string();
+async fn helloworld_handler(Query(QueryParams { name }): Query<QueryParams>) -> String {
+    let mut name_var = "Stranger".to_string();
+    if name.is_some() && !name.to_owned().unwrap().is_empty() {
+        name_var = name.unwrap().to_case(Case::Title);
+    }
+    return "Hello ".to_owned()+&name_var;
 }
 
 #[handler]
 async fn versionz_handler() -> Json<serde_json::Value> {
-    return Json(json!({}));
+    return Json(json!({"name":env!("CARGO_PKG_NAME"),"rev":env!("GIT_SHA")}));
 }
