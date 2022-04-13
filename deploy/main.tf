7f2209ce365f793fcce4c1c431b39078c6a1306f
Balamurali Pandranki <balamurali@live.com>
2022-04-13 13:50:03 +0530

    Add Terraform code

---
+++ deploy/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.5.1"
+    }
+  }
+}
+
+provider "helm" {
+  # Configuration options
+}
+
+resource "helm_release" "nginx_ingress" {
+  name  = "code-challenge"
+  chart = "../helm/code-challenge"
+
+  set {
+    name  = "image.tag"
+    value = var.docker_image_tag
+  }
+}
---
+++ deploy/variables.tf
@@ -0,0 +1,4 @@
+variable "docker_image_tag" {
+  type        = string
+  description = "Docker image tag to deploy"
+}
